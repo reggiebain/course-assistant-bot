0933ac4134bed8a7091b55ca25671762a3b4c501
Reggie Bain <reggiebain@gmail.com>
2024-11-26 21:10:47 -0500

    Update app.py

--- deploy/app.py
+++ deploy/app.py
@@ -260,9 +260,9 @@ if uploaded_file and st.button("Upload and Process Syllabus"):
         print(f"{temp_file=}")
         print(f"{type(temp_file)=}")
         print('hey we did it!!!\n')
-        #docs_processed = load_documents(temp_file)
+        docs_processed = load_documents(temp_file)
         # Split into chunks, load embeddings
-        #knowledge_index = load_embeddings(docs_processed, chunk_size=CHUNK_SIZE)
+        knowledge_index = load_embeddings(docs_processed, chunk_size=CHUNK_SIZE)
         st.success("File uploaded and processed!")
 
     if st.button("Evaluate Syllabus"):
